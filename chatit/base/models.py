f4a1fcc712b1818339ee41b22093aeffc4704b05
Yash <yashraj504300@gmail.com>
2024-01-13 19:41:53 +0530

    CRUD functionality added

--- chatit/base/models.py
+++ chatit/base/models.py
@@ -17,6 +17,9 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created',] #ordering the newest created rooms
+    
 
     def __str__(self):
         return self.name
--- chatit/base/views.py
+++ chatit/base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 # rooms = [
@@ -8,7 +9,6 @@ from .models import Room
 #     {'id':2, 'name':'Design with me'},
 #     {'id':3, 'name':'Frontend  developers'},
 # ]
-
 
 
 def home  (request):
@@ -20,3 +20,37 @@ def room(request, pk):
     room = Room.objects.get(id=pk) #with the help of id we brought back one whole single row
     context = {'room' : room}
     return render(request, 'base/room.html',  context)
+
+def createRoom(request):
+    form = RoomForm()
+
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') #using name parameter 
+        
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj' : room})

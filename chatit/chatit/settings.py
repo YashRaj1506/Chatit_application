0617a3710ac6093510b25f1007f942470acce644
Yash <yashraj504300@gmail.com>
2024-01-15 19:46:42 +0530

    static files

--- chatit/chatit/settings.py
+++ chatit/chatit/settings.py
@@ -121,9 +121,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = "/static_files"
+
+# STATIC_ROOT = 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
